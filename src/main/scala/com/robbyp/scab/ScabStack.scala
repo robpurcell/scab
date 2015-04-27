122d9e73dd898f4253db0a8f94472f40aec3b0e3
Rob Purcell <rob.purcell@gmail.com>
2015-04-27 18:25:42 +0100

    Ad licence file. Add licence headers. Scalastyle violations fixes.

--- src/main/scala/com/robbyp/scab/ScabStack.scala
+++ src/main/scala/com/robbyp/scab/ScabStack.scala
@@ -1,33 +1,51 @@
+// Copyright 2015 Purcell Informatics Limited
+//
+// See the LICENCE file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.robbyp.scab
 
-import org.scalatra._
-import scalate.ScalateSupport
-import org.fusesource.scalate.{ TemplateEngine, Binding }
-import org.fusesource.scalate.layout.DefaultLayoutStrategy
 import javax.servlet.http.HttpServletRequest
-import collection.mutable
 
-trait ScabStack extends ScalatraServlet with ScalateSupport {
+import org.fusesource.scalate.TemplateEngine
+import org.fusesource.scalate.layout.DefaultLayoutStrategy
+import org.scalatra._
+import org.scalatra.scalate.ScalateSupport
 
-  /* wire up the precompiled templates */
+import scala.collection.mutable
+
+trait ScabStack extends ScalatraServlet with ScalateSupport with ApiFormats {
+
+  // wire up the precompiled templates
   override protected def defaultTemplatePath: List[String] = List("/WEB-INF/templates/views")
+
   override protected def createTemplateEngine(config: ConfigT) = {
     val engine = super.createTemplateEngine(config)
     engine.layoutStrategy = new DefaultLayoutStrategy(engine,
       TemplateEngine.templateTypes.map("/WEB-INF/templates/layouts/default." + _): _*)
     engine.packagePrefix = "templates"
     engine
-  }
-  /* end wiring up the precompiled templates */
-  
+  } // end wiring up the precompiled templates
+
   override protected def templateAttributes(implicit request: HttpServletRequest): mutable.Map[String, Any] = {
     super.templateAttributes ++ mutable.Map.empty // Add extra attributes here, they need bindings in the build file
   }
-  
 
   notFound {
     // remove content type in case it was set through an action
-    contentType = null
+    contentType = null // scalastyle:ignore
     // Try to render a ScalateTemplate if no route matched
     findTemplate(requestPath) map { path =>
       contentType = "text/html"
